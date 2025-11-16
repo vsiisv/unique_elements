c44b2755a3ac22699dd5af8f6b41f14a3026cbb1
vsiisv <shores.freezer0u@icloud.com>
2025-11-16 16:07:47 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+list_of_random_numbers = [4, 8, 15, 16, 23, 42, 4, 8, 15, 16, 23, 42]
+unique_numbers = set(list_of_random_numbers)
+print(f"Количество уникальных элементов = {len(unique_numbers)}")
+
+list_of_random_elements = [1, "sdf", 2, "sdf", 3, "sdf", 1, "sdf", 2, "sdf", 3, "sdf"]
+unique_elements = set(list_of_random_elements)
+print(f"Количество уникальных элементов = {len(unique_elements)}")
